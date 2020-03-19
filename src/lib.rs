e4c7efdb50277cd8c947dd7f0f731d44256237e1
sharks <sharks@sharks>
2020-03-19 14:28:10 -0400

    Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -57,3 +57,60 @@ ImplDekuTraits!(u32);
 ImplDekuTraits!(u64);
 ImplDekuTraits!(u128);
 ImplDekuTraits!(usize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[test]
+    fn test_bit_size() {
+        assert_eq!(8, u8::bit_size());
+        assert_eq!(128, u128::bit_size());
+    }
+
+    #[rstest(input,read_bits,expected,expected_rest,
+        case::normal([0xAA, 0xBB, 0xCC, 0xDD].as_ref(), 32, 0xAABBCCDD, ([].as_ref(), 0)),
+        case::normal_offset([0b1001_0110, 0b1110_0000, 0xCC, 0xDD].as_ref(), 12, 0b1001_0110_1110, ([0b1110_0000, 0xCC, 0xDD].as_ref(), 4)),
+        case::too_much_data([0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF].as_ref(), 32, 0xAABBCCDD, ([0xEE, 0xFF].as_ref(), 0)),
+
+        // TODO: Better error message for these
+        #[should_panic(expected="Error((([170, 187], 0), Eof))")]
+        case::not_enough_data([0xAA, 0xBB].as_ref(), 32, 0xFF, ([].as_ref(), 0)),
+    )]
+    fn test_bit_read(input: &[u8], read_bits: usize, expected: u32, expected_rest: (&[u8], usize)) {
+        let res_read = u32::read((input, 0usize), read_bits);
+        assert_eq!(expected, res_read.1);
+        assert_eq!(expected_rest, res_read.0);
+    }
+
+    #[rstest(input,expected,
+        case::normal(0xAABBCCDD, vec![0xDD, 0xCC, 0xBB, 0xAA]),
+    )]
+    fn test_bit_write(input: u32, expected: Vec<u8>) {
+        let res_write = input.write();
+        assert_eq!(expected, res_write);
+    }
+
+    #[rstest(input,read_bits,expected,expected_rest,expected_write,
+        case::normal([0xAA, 0xBB, 0xCC, 0xDD].as_ref(), 32, 0xAABBCCDD, ([].as_ref(), 0), vec![0xDD, 0xCC, 0xBB, 0xAA]),
+    )]
+    fn test_bit_read_write(
+        input: &[u8],
+        read_bits: usize,
+        expected: u32,
+        expected_rest: (&[u8], usize),
+        expected_write: Vec<u8>,
+    ) {
+        let res_read = u32::read((input, 0usize), read_bits);
+        assert_eq!(expected, res_read.1);
+        assert_eq!(expected_rest, res_read.0);
+
+        let res_write = res_read.1.write();
+        assert_eq!(expected_write, res_write);
+
+        // BUG: This should pass?
+        //assert_eq!(input[..expected_write.len()].to_vec(), expected_write);
+    }
+}

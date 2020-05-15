e211c5bd02ff5b2faaae2864cce9d7f8221b2192
Emmanuel Thompson <eet6646@gmail.com>
2020-05-15 16:28:11 -0400

    Implement BitsReader/Writer traits on Vec<$typ>
    This makes it so they are treated as a normal Deku type

--- deku-derive/src/macros/deku_write.rs
+++ deku-derive/src/macros/deku_write.rs
@@ -1,12 +1,9 @@
 use crate::DekuReceiver;
 use proc_macro2::TokenStream;
 use quote::quote;
-use std::collections::{HashMap, HashSet};
 
 pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darling::Error> {
     let mut tokens = TokenStream::new();
-    let mut seen_field_names: HashSet<String> = HashSet::new();
-    let mut vec_len_fields = HashMap::new();
     let ident = &input.ident;
 
     let fields = input
@@ -16,36 +13,32 @@ pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darli
         .expect("expected `struct` type")
         .fields;
 
-    // Extract fields that represent Vec lenghts
-    for (i, f) in fields.iter().enumerate() {
-        if let Some(ref field_len_name) = f.vec_len {
-            vec_len_fields.insert(
-                field_len_name,
-                f.ident.as_ref().map(|v| quote!(#v)).unwrap_or_else(|| {
-                    let ret = syn::Index::from(i);
-                    quote! { #ret }
-                }),
-            );
-        }
-    }
-
     let field_writes: Result<Vec<_>, _> = fields
         .into_iter()
         .enumerate()
         .map(|(i, f)| {
-            let mut field_type = &f.ty;
+            let field_type = {
+                let t = &f.ty;
+                quote! { <#t> }
+            };
             let field_endian = f.endian.unwrap_or(input.endian);
             let field_bits = f.bits;
             let field_bytes = f.bytes;
-            let field_vec_len = &f.vec_len;
-            let mut vec_ident = None;
+            let field_len = &f
+                .len
+                .as_ref()
+                .map(|v| syn::Ident::new(&v, syn::export::Span::call_site()));
+            let field_len = if let Some(v) = field_len {
+                quote! { * input.#v as usize }
+            } else {
+                quote! {}
+            };
 
             // Support named or indexed fields
-            let mut field_ident = f.ident.as_ref().map(|v| quote!(#v)).unwrap_or_else(|| {
+            let field_ident = f.ident.as_ref().map(|v| quote!(#v)).unwrap_or_else(|| {
                 let ret = syn::Index::from(i);
                 quote! { #ret }
             });
-            let field_name = field_ident.to_string();
 
             if field_bits.is_some() && field_bytes.is_some() {
                 return Err(darling::Error::duplicate_field(
@@ -55,28 +48,6 @@ pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darli
 
             let endian_flip = field_endian != input.endian;
 
-            if let Some(len_field_name) = field_vec_len {
-                vec_ident = Some(super::validate_vec_len(
-                    &mut field_ident,
-                    &mut field_type,
-                    len_field_name,
-                    &mut seen_field_names,
-                )?);
-            } else {
-                seen_field_names.insert(field_ident.to_string());
-
-                // If this field contains the length of a vec
-                if let Some(vec_ident) = vec_len_fields.get(&field_name) {
-                    field_ident = quote! {
-                        input.#vec_ident.len()
-                    };
-                } else {
-                    field_ident = quote! {
-                        input.#field_ident
-                    };
-                }
-            }
-
             let field_bits = field_bits.or_else(|| field_bytes.map(|v| v * 8usize));
             let field_bits = if field_bits.is_some() {
                 quote! { #field_bits }
@@ -84,37 +55,26 @@ pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darli
                 quote! { #field_type::bit_size() }
             };
 
-            let mut field_write = quote! {
+            let field_write = quote! {
                 // TODO: Can this somehow be compile time?
                 // Assert if we're writing more then what the type supports
                 assert!(#field_bits <= #field_type::bit_size());
 
                 let field_val = if (#endian_flip) {
-                    #field_ident
+                    input.#field_ident
                 } else {
-                    #field_ident.swap_endian()
+                    input.#field_ident.swap_endian()
                 };
 
                 let field_bytes = field_val.write();
 
                 // Reverse to write from MSB -> LSB
-                for i in (0..#field_bits).rev() {
+                for i in (0..#field_bits #field_len).rev() {
                     let field_val = field_bytes[i/8];
                     let bit = (field_val & 1 << (i%8)) != 0;
                     acc.push(bit)
                 }
             };
-
-            // if this is a vec of field_type
-            if let Some(vec_name) = vec_ident {
-                field_write = quote! {
-                    for #field_ident in input.#vec_name.iter() {
-                        let #field_ident = *#field_ident;
-                        #field_write
-                    }
-                };
-            }
-            //println!("{}", field_write.to_string());
 
             Ok(field_write)
         })
@@ -159,5 +119,6 @@ pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darli
         }
     });
 
+    // println!("{}", tokens.to_string());
     Ok(tokens)
 }

80e875b5bb37816887ff218008f7b63bb8af7309
Emmanuel Thompson <eet6646@gmail.com>
2020-05-19 17:41:19 -0400

    refactor write to allow dynamic field changes

--- deku-derive/src/macros/deku_write.rs
+++ deku-derive/src/macros/deku_write.rs
@@ -13,77 +13,115 @@ pub(crate) fn emit_deku_write(input: &DekuReceiver) -> Result<TokenStream, darli
         .expect("expected `struct` type")
         .fields;
 
-    let field_writes: Result<Vec<_>, _> = fields
-        .into_iter()
-        .enumerate()
-        .map(|(i, f)| {
-            let field_type = {
-                let t = &f.ty;
-                quote! { <#t> }
-            };
-            let field_endian = f.endian.unwrap_or(input.endian);
-            let field_bits = f.bits;
-            let field_bytes = f.bytes;
-            let field_len = &f
-                .len
-                .as_ref()
-                .map(|v| syn::Ident::new(&v, syn::export::Span::call_site()));
+    let mut field_writes = vec![];
+    let mut field_inputs = vec![];
 
-            // Support named or indexed fields
-            let field_ident = f.ident.as_ref().map(|v| quote!(#v)).unwrap_or_else(|| {
-                let ret = syn::Index::from(i);
-                quote! { #ret }
-            });
+    for (i, f) in fields.into_iter().enumerate() {
+        let field_type = {
+            let t = &f.ty;
+            quote! { <#t> }
+        };
+        let field_endian = f.endian.unwrap_or(input.endian);
+        let field_bits = f.bits;
+        let field_bytes = f.bytes;
 
-            if field_bits.is_some() && field_bytes.is_some() {
-                return Err(darling::Error::duplicate_field(
-                    "both \"bits\" and \"bytes\" specified",
-                ));
+        let field_len = &f
+            .len
+            .as_ref()
+            .map(|v| syn::Ident::new(&v, syn::export::Span::call_site()));
+
+        // Support named or indexed fields
+        let (field_ident_orig, field_ident) = f
+            .ident
+            .as_ref()
+            .map(|v| quote!(#v))
+            .or_else(|| {
+                Some({
+                    let i = syn::Index::from(i);
+                    quote!(#i)
+                })
+            })
+            .and_then(|v| {
+                let field_ident = syn::Ident::new(
+                    &format!("field_{}", quote! { #v }),
+                    syn::export::Span::call_site(),
+                );
+
+                Some((quote! {#v}, quote! { #field_ident }))
+            })
+            .unwrap();
+
+        // If `len` attr is provided, overwrite the field with the .len() of the container
+        let field_input_len_overwrite = field_len.as_ref().map(|v| {
+            let len_field_ident = syn::Ident::new(
+                &format!("field_{}", quote! { #v }), // TODO: duplicate code
+                syn::export::Span::call_site(),
+            );
+
+            quote! {
+                let #len_field_ident = #field_ident.len();
             }
+        });
 
-            let endian_flip = field_endian != input.endian;
+        let field_input = quote! {
+            let #field_ident = input.#field_ident_orig;
 
-            let field_bits = field_bits.or_else(|| field_bytes.map(|v| v * 8usize));
-            let field_bits = if field_bits.is_some() {
-                quote! { #field_bits }
+            #field_input_len_overwrite
+        };
+        field_inputs.push(field_input);
+
+        if field_bits.is_some() && field_bytes.is_some() {
+            return Err(darling::Error::duplicate_field(
+                "both \"bits\" and \"bytes\" specified",
+            ));
+        }
+
+        let endian_flip = field_endian != input.endian;
+
+        let field_bits = field_bits.or_else(|| field_bytes.map(|v| v * 8usize));
+        let field_bits = if field_bits.is_some() {
+            quote! { #field_bits }
+        } else {
+            quote! { #field_type::bit_size() }
+        };
+
+        let mul_len = if let Some(v) = field_len {
+            let len_field_ident = syn::Ident::new(
+                &format!("field_{}", quote! { #v }), // TODO: duplicate code
+                syn::export::Span::call_site(),
+            );
+            quote! { * #len_field_ident as usize }
+        } else {
+            quote! {}
+        };
+
+        let field_write = quote! {
+            // TODO: Can this somehow be compile time?
+            // Assert if we're writing more then what the type supports
+            assert!(#field_bits <= #field_type::bit_size());
+
+            let field_val = if (#endian_flip) {
+                #field_ident.swap_endian()
             } else {
-                quote! { #field_type::bit_size() }
+                #field_ident
             };
 
-            let mul_len = if let Some(v) = field_len {
-                quote! { * input.#v as usize }
-            } else {
-                quote! {}
-            };
+            let field_bytes = field_val.write();
 
-            let field_write = quote! {
-                // TODO: Can this somehow be compile time?
-                // Assert if we're writing more then what the type supports
-                assert!(#field_bits <= #field_type::bit_size());
+            let mut bits: BitVec<P, u8> = field_bytes.into();
+            let index = bits.len() - #field_bits #mul_len;
+            acc.extend_from_slice(&bits.as_bitslice()[index..]);
+        };
 
-                let field_val = if (#endian_flip) {
-                    input.#field_ident.swap_endian()
-                } else {
-                    input.#field_ident
-                };
-
-                let field_bytes = field_val.write();
-
-                let mut bits: BitVec<P, u8> = field_bytes.into();
-                let index = bits.len() - #field_bits #mul_len;
-                acc.extend_from_slice(&bits.as_bitslice()[index..]);
-            };
-
-            Ok(field_write)
-        })
-        .collect();
-
-    let field_writes = field_writes?;
+        field_writes.push(field_write);
+    }
 
     tokens.extend(quote! {
         impl<P> From<#ident> for BitVec<P, u8> where P: BitOrder {
             fn from(input: #ident) -> Self {
                 let mut acc: BitVec<P, u8> = BitVec::new();
+
+                #(#field_inputs)*
 
                 #(#field_writes)*
 

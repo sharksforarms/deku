890f760e3bad1f8cacfbd9a91296acceb09dfd08
ElasT0ny <elast0ny00@gmail.com>
2020-05-21 15:04:15 -0400

    Removed BitsSize trait

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use crate::{
-    error::DekuError, BitsReader, BitsReaderItems, BitsSize, BitsWriter, DekuRead, DekuWrite,
+    error::DekuError, BitsReader, BitsReaderItems, BitsWriter, DekuRead, DekuWrite,
 };
 pub use bitvec::{
     order::BitOrder, order::Lsb0, order::Msb0, slice::AsBits, slice::BitSlice, vec::BitVec,

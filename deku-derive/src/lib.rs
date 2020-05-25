2a0df7dc0c9287e2f88cb4c10a79e5a72b2c118d
Emmanuel Thompson <eet6646@gmail.com>
2020-05-25 16:14:46 -0400

    Refactor reader, support for enums

--- deku-derive/src/lib.rs
+++ deku-derive/src/lib.rs
@@ -1,9 +1,6 @@
-use darling::ast;
-use darling::FromDeriveInput;
-use darling::FromField;
-use darling::FromMeta;
+use darling::{ast, FromDeriveInput, FromField, FromMeta, FromVariant};
 use proc_macro2::TokenStream;
-
+use quote::quote;
 mod macros;
 use crate::macros::{deku_read::emit_deku_read, deku_write::emit_deku_write};
 
@@ -27,16 +24,58 @@ impl Default for EndianNess {
 }
 
 #[derive(Debug, FromDeriveInput)]
-// Process all `deku` attributes and only support structs
-#[darling(attributes(deku), supports(struct_any))]
+#[darling(
+    attributes(deku),
+    supports(struct_any, enum_any),
+    map = "DekuReceiver::init"
+)]
 struct DekuReceiver {
     ident: syn::Ident,
     generics: syn::Generics,
-    data: ast::Data<(), DekuFieldReceiver>,
+    data: ast::Data<DekuVariantReceiver, DekuFieldReceiver>,
 
-    // Default EndianNess
     #[darling(default)]
     endian: EndianNess,
+
+    #[darling(default)]
+    id_type: Option<syn::Ident>,
+
+    #[darling(default)]
+    id_bits: Option<usize>,
+
+    #[darling(default)]
+    id_bytes: Option<usize>,
+}
+
+impl DekuReceiver {
+    fn init(self) -> Self {
+        if (self.id_type.is_some() || self.id_bits.is_some() || self.id_bytes.is_some())
+            && !self.data.is_enum()
+        {
+            panic!("`id_*` attributes only supported on enum")
+        }
+
+        // Validate that `id_type` is set with size
+        if (self.id_bits.is_some() || self.id_bytes.is_some()) && self.id_type.is_none() {
+            panic!("`id_type` must be specified with `id_bits` or `id_bytes`");
+        }
+
+        // Validate `id_bits` and `id_bytes` attributes
+        if self.id_bits.is_some() && self.id_bytes.is_some() {
+            panic!("conflicting: both \"id_bits\" and \"id_bytes\" specified on field");
+        }
+
+        // Calculate bit size from both attributes
+        let id_bits = self.id_bits.or_else(|| self.id_bytes.map(|v| v * 8));
+        let id_bytes = None;
+
+        // Return updated receiver
+        Self {
+            id_bits,
+            id_bytes,
+            ..self
+        }
+    }
 }
 
 impl DekuReceiver {
@@ -49,8 +88,27 @@ impl DekuReceiver {
     }
 }
 
+fn option_as_tokenstream(input: Option<String>) -> Option<TokenStream> {
+    input.map(|v| {
+        v.parse::<TokenStream>()
+            .expect("could not parse token stream")
+    })
+}
+
+fn gen_field_ident<T: ToString>(ident: Option<T>, index: usize) -> syn::Ident {
+    let field_name = match ident {
+        Some(field_name) => field_name.to_string(),
+        None => {
+            let index = syn::Index::from(index);
+            format!("field_{}", quote! { #index })
+        }
+    };
+
+    syn::Ident::new(&field_name, syn::export::Span::call_site())
+}
+
 #[derive(Debug, FromField)]
-#[darling(attributes(deku))]
+#[darling(attributes(deku), map = "DekuFieldReceiver::init")]
 struct DekuFieldReceiver {
     ident: Option<syn::Ident>,
     ty: syn::Type,
@@ -67,11 +125,82 @@ struct DekuFieldReceiver {
     #[darling(default)]
     bytes: Option<usize>,
 
-    #[darling(default)]
-    reader: Option<String>,
+    #[darling(default, map = "option_as_tokenstream")]
+    reader: Option<TokenStream>,
+
+    #[darling(default, map = "option_as_tokenstream")]
+    writer: Option<TokenStream>,
+}
+
+impl DekuFieldReceiver {
+    fn init(self) -> Self {
+        // Validate `bits` and `bytes` attributes
+        if self.bits.is_some() && self.bytes.is_some() {
+            panic!("conflicting: both \"bits\" and \"bytes\" specified on field");
+        }
+
+        // Calculate bit size from both attributes
+        let bits = self.bits.or_else(|| self.bytes.map(|v| v * 8));
+        let bytes = None;
+
+        // Return updated receiver
+        Self {
+            bits,
+            bytes,
+            ..self
+        }
+    }
+
+    fn is_named(&self) -> bool {
+        self.ident.is_some()
+    }
+
+    fn get_ident(&self, i: usize) -> TokenStream {
+        let field_ident = gen_field_ident(self.ident.as_ref(), i);
+        quote! { #field_ident }
+    }
+
+    fn get_len_field(&self, i: usize) -> Option<TokenStream> {
+        self.len
+            .as_ref()
+            .map(|field_len| {
+                if self.is_named() {
+                    gen_field_ident(Some(field_len), i)
+                } else {
+                    let index = field_len.parse::<usize>().unwrap_or_else(|_| {
+                        panic!("could not parse `len` attribute as unnamed: {}", field_len)
+                    });
+                    gen_field_ident(None::<String>, index)
+                }
+            })
+            .map(|field_len| {
+                quote! { #field_len }
+            })
+    }
+}
+
+#[derive(Debug, FromVariant)]
+#[darling(attributes(deku), map = "DekuVariantReceiver::init")]
+struct DekuVariantReceiver {
+    ident: syn::Ident,
+    fields: ast::Fields<DekuFieldReceiver>,
 
     #[darling(default)]
-    writer: Option<String>,
+    endian: Option<EndianNess>,
+
+    #[darling(default, map = "option_as_tokenstream")]
+    reader: Option<TokenStream>,
+
+    #[darling(default, map = "option_as_tokenstream")]
+    writer: Option<TokenStream>,
+
+    id: String,
+}
+
+impl DekuVariantReceiver {
+    fn init(self) -> Self {
+        self
+    }
 }
 
 #[proc_macro_derive(DekuRead, attributes(deku))]
@@ -95,7 +224,7 @@ mod tests {
     use syn::parse_str;
 
     #[rstest(input,
-        // Valid
+        // Valid struct
         case::struct_unnamed(r#"struct Test(u8, u8);"#),
         case::struct_unnamed_attrs(r#"struct Test(#[deku(bits=4)] u8, u8);"#),
         case::struct_all_attrs(r#"
@@ -110,17 +239,22 @@ mod tests {
             field_d: u32,
         }"#),
 
-        // Invalid
-        #[should_panic(expected = "UnsupportedShape(\"enum\")")]
-        case::invalid_enum(r#"enum Test { A }"#),
+        // Invalid Struct
         #[should_panic(expected = "UnknownField(ErrorUnknownField { name: \"sbits\", did_you_mean: Some(\"bits\") })")]
         case::invalid_field(r#"struct Test(#[deku(sbits=4)] u8);"#),
         #[should_panic(expected = "DuplicateField(\"bits\")")]
         case::invalid_field_duplicate(r#"struct Test(#[deku(bits=4, bits=5)] u8);"#),
-        #[should_panic(expected = "DuplicateField(\"both \\\"bits\\\" and \\\"bytes\\\" specified\")")]
+        #[should_panic(expected = "conflicting: both \"bits\" and \"bytes\" specified on field")]
         case::invalid_field_bitsnbytes(r#"struct Test(#[deku(bits=4, bytes=1)] u8);"#),
 
-        // TODO: these tests should error/warn eventually
+        // Valid Enum
+        case::enum_unnamed(r#"#[deku(id_type=u8)] enum Test { A }"#),
+
+        // Invalid Enum
+        #[should_panic(expected = "expected `id_type` on enum")]
+        case::invalid_expected_id_type(r#"enum Test { A }"#),
+
+        // TODO: these tests should error/warn eventually?
         // error: trying to store 9 bits in 8 bit type
         case::invalid_storage(r#"struct Test(#[deku(bits=9)] u8);"#),
         // warn: trying to set endian on a type which wouldn't make a difference

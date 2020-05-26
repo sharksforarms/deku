7569e003ed7a72b1acad86b55e87d592f6145b9c
Emmanuel Thompson <eet6646@gmail.com>
2020-05-26 12:43:22 -0400

    refactor "overwrites" to "updates" into it's own
    - This allows us to have non &mut writes

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
-pub use crate::{error::DekuError, BitsReader, BitsReaderItems, BitsWriter, DekuRead, DekuWrite};
+pub use crate::{
+    error::DekuError, BitsReader, BitsReaderItems, BitsWriter, DekuRead, DekuWrite, DekuWriteApi,
+};
 pub use bitvec::{
     order::BitOrder, order::Lsb0, order::Msb0, slice::AsBits, slice::BitSlice, vec::BitVec,
 };

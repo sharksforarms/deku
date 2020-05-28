efee0b74225ab2d0f17a2d32c4b38d33087c72ef
Emmanuel Thompson <eet6646@gmail.com>
2020-05-28 14:50:53 -0400

    cleanup and add some docs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,4 @@
-pub use crate::{
-    error::DekuError, BitsReader, BitsReaderItems, BitsWriter, DekuRead, DekuWrite, DekuWriteApi,
-};
+pub use crate::{error::DekuError, BitsReader, BitsReaderItems, BitsWriter, DekuRead, DekuWrite};
 pub use bitvec::{
     order::BitOrder, order::Lsb0, order::Msb0, slice::AsBits, slice::BitSlice, vec::BitVec,
 };

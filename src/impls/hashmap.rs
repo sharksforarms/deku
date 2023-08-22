953847f769e6a77c8422c0247adbb4b24a4a7a27
wcampbell <wcampbell1995@gmail.com>
2023-08-21 22:10:51 -0400

    Impl DekuReader for HashMap

--- src/impls/hashmap.rs
+++ src/impls/hashmap.rs
@@ -1,12 +1,11 @@
-// TODO: impl DekuReader
-
 use std::collections::HashMap;
 use std::hash::{BuildHasher, Hash};
 
+use acid_io::Read;
 use bitvec::prelude::*;
 
 use crate::ctx::*;
-use crate::{DekuError, DekuRead, DekuWrite};
+use crate::{DekuError, DekuRead, DekuReader, DekuWrite};
 
 /// Read `K, V`s into a hashmap until a given predicate returns true
 /// * `capacity` - an optional capacity to pre-allocate the hashmap with
@@ -48,6 +47,41 @@ where
     }
 
     Ok((total_read, res))
+}
+
+/// Read `K, V`s into a hashmap until a given predicate returns true
+/// * `capacity` - an optional capacity to pre-allocate the hashmap with
+/// * `ctx` - The context required by `K, V`. It will be passed to every `K, V` when constructing.
+/// * `predicate` - the predicate that decides when to stop reading `K, V`s
+/// The predicate takes two parameters: the number of bits that have been read so far,
+/// and a borrow of the latest value to have been read. It should return `true` if reading
+/// should now stop, and `false` otherwise
+#[allow(clippy::type_complexity)]
+fn from_reader_hashmap_with_predicate<'a, K, V, S, Ctx, Predicate, R: Read>(
+    container: &mut crate::container::Container<R>,
+    capacity: Option<usize>,
+    ctx: Ctx,
+    mut predicate: Predicate,
+) -> Result<HashMap<K, V, S>, DekuError>
+where
+    K: DekuReader<'a, Ctx> + Eq + Hash,
+    V: DekuReader<'a, Ctx>,
+    S: BuildHasher + Default,
+    Ctx: Copy,
+    Predicate: FnMut(usize, &(K, V)) -> bool,
+{
+    let mut res = HashMap::with_capacity_and_hasher(capacity.unwrap_or(0), S::default());
+
+    let mut found_predicate = false;
+    let orig_bits_read = container.bits_read;
+
+    while !found_predicate {
+        let val = <(K, V)>::from_reader(container, ctx)?;
+        found_predicate = predicate(container.bits_read - orig_bits_read, &val);
+        res.insert(val.0, val.1);
+    }
+
+    Ok(res)
 }
 
 impl<'a, K, V, S, Ctx, Predicate> DekuRead<'a, (Limit<(K, V), Predicate>, Ctx)> for HashMap<K, V, S>
@@ -121,6 +155,91 @@ where
     }
 }
 
+impl<'a, K, V, S, Ctx, Predicate> DekuReader<'a, (Limit<(K, V), Predicate>, Ctx)>
+    for HashMap<K, V, S>
+where
+    K: DekuReader<'a, Ctx> + Eq + Hash,
+    V: DekuReader<'a, Ctx>,
+    S: BuildHasher + Default,
+    Ctx: Copy,
+    Predicate: FnMut(&(K, V)) -> bool,
+{
+    /// Read `T`s until the given limit
+    /// * `limit` - the limiting factor on the amount of `T`s to read
+    /// * `inner_ctx` - The context required by `T`. It will be passed to every `T`s when constructing.
+    /// # Examples
+    /// ```rust
+    /// # use deku::ctx::*;
+    /// # use deku::DekuReader;
+    /// # use std::collections::HashMap;
+    /// # use std::io::Cursor;
+    /// let mut input = Cursor::new(vec![100, 1, 2, 3, 4]);
+    /// let mut container = deku::container::Container::new(&mut input);
+    /// let map =
+    ///     HashMap::<u8, u32>::from_reader(&mut container, (1.into(), Endian::Little)).unwrap();
+    /// let mut expected = HashMap::<u8, u32>::default();
+    /// expected.insert(100, 0x04030201);
+    /// assert_eq!(expected, map)
+    /// ```
+    fn from_reader<R: Read>(
+        container: &mut crate::container::Container<R>,
+        (limit, inner_ctx): (Limit<(K, V), Predicate>, Ctx),
+    ) -> Result<Self, DekuError>
+    where
+        Self: Sized,
+    {
+        match limit {
+            // Read a given count of elements
+            Limit::Count(mut count) => {
+                // Handle the trivial case of reading an empty hashmap
+                if count == 0 {
+                    return Ok(HashMap::<K, V, S>::default());
+                }
+
+                // Otherwise, read until we have read `count` elements
+                from_reader_hashmap_with_predicate(
+                    container,
+                    Some(count),
+                    inner_ctx,
+                    move |_, _| {
+                        count -= 1;
+                        count == 0
+                    },
+                )
+            }
+
+            // Read until a given predicate returns true
+            Limit::Until(mut predicate, _) => {
+                from_reader_hashmap_with_predicate(container, None, inner_ctx, move |_, kv| {
+                    predicate(kv)
+                })
+            }
+
+            // Read until a given quantity of bits have been read
+            Limit::BitSize(size) => {
+                let bit_size = size.0;
+                from_reader_hashmap_with_predicate(
+                    container,
+                    None,
+                    inner_ctx,
+                    move |read_bits, _| read_bits == bit_size,
+                )
+            }
+
+            // Read until a given quantity of byte bits have been read
+            Limit::ByteSize(size) => {
+                let bit_size = size.0 * 8;
+                from_reader_hashmap_with_predicate(
+                    container,
+                    None,
+                    inner_ctx,
+                    move |read_bits, _| read_bits == bit_size,
+                )
+            }
+        }
+    }
+}
+
 impl<'a, K, V, S, Predicate> DekuRead<'a, Limit<(K, V), Predicate>> for HashMap<K, V, S>
 where
     K: DekuRead<'a> + Eq + Hash,
@@ -137,6 +256,25 @@ where
         Self: Sized,
     {
         Self::read(input, (limit, ()))
+    }
+}
+
+impl<'a, K, V, S, Predicate> DekuReader<'a, Limit<(K, V), Predicate>> for HashMap<K, V, S>
+where
+    K: DekuReader<'a> + Eq + Hash,
+    V: DekuReader<'a>,
+    S: BuildHasher + Default,
+    Predicate: FnMut(&(K, V)) -> bool,
+{
+    /// Read `K, V`s until the given limit from input for types which don't require context.
+    fn from_reader<R: Read>(
+        container: &mut crate::container::Container<R>,
+        limit: Limit<(K, V), Predicate>,
+    ) -> Result<Self, DekuError>
+    where
+        Self: Sized,
+    {
+        Self::from_reader(container, (limit, ()))
     }
 }
 
@@ -168,8 +306,11 @@ impl<K: DekuWrite<Ctx>, V: DekuWrite<Ctx>, S, Ctx: Copy> DekuWrite<Ctx> for Hash
 
 #[cfg(test)]
 mod tests {
+    use acid_io::Cursor;
     use rstest::rstest;
     use rustc_hash::FxHashMap;
+
+    use crate::container::Container;
 
     use super::*;
 
@@ -210,7 +351,7 @@ mod tests {
         #[should_panic(expected = "Parse(\"too much data: container of 8 bits cannot hold 9 bits\")")]
         case::too_much_data([0xAA, 0xBB].as_ref(), Endian::Little, Some(9), 1.into(), FxHashMap::default(), bits![u8, Msb0;]),
     )]
-    fn test_hashmap_read<Predicate: FnMut(&(u8, u8)) -> bool>(
+    fn test_hashmap_read<Predicate: FnMut(&(u8, u8)) -> bool + Copy>(
         input: &[u8],
         endian: Endian,
         bit_size: Option<usize>,
@@ -229,6 +370,18 @@ mod tests {
 
         assert_eq!(expected, res_read);
         assert_eq!(expected_rest, bit_slice[amt_read..]);
+
+        let mut cursor = Cursor::new(input);
+        let mut container = Container::new(&mut cursor);
+        let res_read = match bit_size {
+            Some(bit_size) => FxHashMap::<u8, u8>::from_reader(
+                &mut container,
+                (limit, (endian, BitSize(bit_size))),
+            )
+            .unwrap(),
+            None => FxHashMap::<u8, u8>::from_reader(&mut container, (limit, (endian))).unwrap(),
+        };
+        assert_eq!(expected, res_read);
     }
 
     #[rstest(input, endian, expected,
@@ -249,7 +402,7 @@ mod tests {
         case::bytes_le([0xAA, 0xBB, 0, 0xCC, 0xDD, 0].as_ref(), Endian::Little, BitSize(24).into(), fxhashmap!{0xBBAA => 0}, bits![u8, Msb0; 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], vec![0xAA, 0xBB, 0]),
         case::bytes_be([0xAA, 0xBB, 0, 0xCC, 0xDD, 0].as_ref(), Endian::Big, BitSize(24).into(), fxhashmap!{0xAABB => 0}, bits![u8, Msb0; 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], vec![0xAA, 0xBB, 0]),
     )]
-    fn test_hashmap_read_write<Predicate: FnMut(&(u16, u8)) -> bool>(
+    fn test_hashmap_read_write<Predicate: FnMut(&(u16, u8)) -> bool + Copy>(
         input: &[u8],
         endian: Endian,
         limit: Limit<(u16, u8), Predicate>,
@@ -262,6 +415,11 @@ mod tests {
         let (amt_read, res_read) = FxHashMap::<u16, u8>::read(bit_slice, (limit, endian)).unwrap();
         assert_eq!(expected, res_read);
         assert_eq!(expected_rest, bit_slice[amt_read..]);
+
+        let mut cursor = Cursor::new(input);
+        let mut container = Container::new(&mut cursor);
+        let res_read = FxHashMap::<u16, u8>::from_reader(&mut container, (limit, endian)).unwrap();
+        assert_eq!(expected, res_read);
 
         let mut res_write = bitvec![u8, Msb0;];
         res_read.write(&mut res_write, endian).unwrap();

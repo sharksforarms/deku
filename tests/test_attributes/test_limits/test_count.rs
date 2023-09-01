57de454bfdadcce3551c1f7098cd0ff453e87c90
Caleb Fletcher <caleb@fletcher.cf>
2023-09-01 15:02:36 +1000

    test: add zero-count test

--- tests/test_attributes/test_limits/test_count.rs
+++ tests/test_attributes/test_limits/test_count.rs
@@ -51,6 +51,28 @@ mod test_slice {
     }
 
     #[test]
+    fn test_count_zero() {
+        #[derive(PartialEq, Debug, DekuRead, DekuWrite)]
+        struct TestStruct<'a> {
+            #[deku(count = "0")]
+            data: &'a [u8],
+        }
+
+        let test_data: Vec<u8> = [].to_vec();
+
+        let ret_read = TestStruct::try_from(test_data.as_ref()).unwrap();
+        assert_eq!(
+            TestStruct {
+                data: test_data.as_ref()
+            },
+            ret_read
+        );
+
+        let ret_write: Vec<u8> = ret_read.try_into().unwrap();
+        assert_eq!(test_data, ret_write);
+    }
+
+    #[test]
     #[should_panic(expected = "Incomplete(NeedSize { bits: 8 })")]
     fn test_count_error() {
         #[derive(PartialEq, Debug, DekuRead, DekuWrite)]
@@ -110,6 +132,23 @@ mod test_vec {
             },
             ret_read
         );
+
+        let ret_write: Vec<u8> = ret_read.try_into().unwrap();
+        assert_eq!(test_data, ret_write);
+    }
+
+    #[test]
+    fn test_count_zero() {
+        #[derive(PartialEq, Debug, DekuRead, DekuWrite)]
+        struct TestStruct {
+            #[deku(count = "0")]
+            data: Vec<u8>,
+        }
+
+        let test_data: Vec<u8> = [].to_vec();
+
+        let ret_read = TestStruct::try_from(test_data.as_ref()).unwrap();
+        assert_eq!(TestStruct { data: vec![] }, ret_read);
 
         let ret_write: Vec<u8> = ret_read.try_into().unwrap();
         assert_eq!(test_data, ret_write);

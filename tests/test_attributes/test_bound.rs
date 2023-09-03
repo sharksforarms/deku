b4707728e95e79d5eb61f292c14e3e9591e6e82f
MegaBluejay <dmoiseev2011@gmail.com>
2023-09-03 20:42:52 +0300

    Simplify bound test

--- tests/test_attributes/test_bound.rs
+++ tests/test_attributes/test_bound.rs
@@ -1,27 +1,16 @@
 use std::convert::{TryFrom, TryInto};
-use std::marker::PhantomData;
 
 use deku::prelude::*;
 
 #[test]
 fn test_bound() {
     #[derive(PartialEq, Debug, DekuRead, DekuWrite)]
-    #[deku(bound = "T: DekuRead<'a, ()>, T: DekuWrite<()>")]
-    struct GenericType<'a, T> {
-        t: T,
-        #[deku(skip)]
-        phantom: PhantomData<&'a ()>,
-    }
+    #[deku(bound = "for<'a> T: DekuRead<'a, ()> + DekuWrite<()>")]
+    struct GenericType<T>(T);
 
     let test_data = [0x01_u8];
     let ret_read = <GenericType<u8>>::try_from(&test_data[..]).unwrap();
-    assert_eq!(
-        ret_read,
-        GenericType {
-            t: 0x01,
-            phantom: PhantomData
-        }
-    );
+    assert_eq!(ret_read, GenericType(0x01));
 
     let ret_write: Vec<u8> = ret_read.try_into().unwrap();
     assert_eq!(ret_write, test_data);

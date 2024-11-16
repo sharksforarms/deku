704f7c7198d2042b2a05badbe8792060d56c8f77
Pascal Sommer <Pascal-So@users.noreply.github.com>
2024-11-16 20:12:56 +0100

    fix docs: type->id_type (#494)

--- deku-derive/src/macros/deku_write.rs
+++ deku-derive/src/macros/deku_write.rs
@@ -271,7 +271,7 @@ fn emit_enum(input: &DekuData) -> Result<TokenStream, syn::Error> {
                 ));
             }
         } else {
-            // either `id` or `type` needs to be specified
+            // either `id` or `id_type` needs to be specified
             unreachable!();
         };
 

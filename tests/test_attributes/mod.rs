55ed5f6bcfb1cd0f8edc8b061ea0c08f29ad817a
goto40 <pierre.bayerl@googlemail.com>
2025-05-27 19:35:26 +0200

    fix: clippy fmt

--- tests/test_attributes/mod.rs
+++ tests/test_attributes/mod.rs
@@ -1,5 +1,7 @@
 mod test_assert;
 mod test_assert_eq;
+#[cfg(feature = "bits")]
+mod test_bitfield_values_range_check;
 mod test_cond;
 mod test_ctx;
 mod test_limits;
@@ -9,6 +11,4 @@ mod test_skip;
 mod test_temp;
 #[cfg(feature = "bits")]
 mod test_temp_value_with_cond;
-#[cfg(feature = "bits")]
-mod test_bitfield_values_range_check;
 mod test_update;

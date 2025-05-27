42aea0d8b80beb0aef83ab2f034c8124ce4c13c4
goto40 <pierre.bayerl@googlemail.com>
2025-05-27 19:20:42 +0200

    chore: extended tests (trivial cases)

--- tests/test_attributes/test_bitfield_values_range_check.rs
+++ tests/test_attributes/test_bitfield_values_range_check.rs
@@ -203,3 +203,129 @@ fn check_little_signed_i10_decode_encode_negative_value() {
     test_struct.b = -513;
     assert!(test_struct.to_bytes().is_err());
 }
+
+#[test]
+fn check_big_unsigned_u16_decode_encode_positive_value() {
+    #[derive(Debug, PartialEq, Default, Clone, DekuRead, DekuWrite)]
+    pub struct TestStruct {
+        #[deku(bits = "1")]
+        pub a: bool,
+        #[deku(pad_bits_before = "15", bits = "16", endian = "big")]
+        pub b: u16,
+    }
+
+    let buffer = vec![0b10000000, 0b00000000, 0b00000000, 0b00000010];
+
+    let ((remaining_bytes, offset), mut test_struct) =
+        TestStruct::from_bytes((&buffer, 0)).expect("decoder error");
+
+    // everything consumed?
+    assert_eq!(offset, 0);
+    assert_eq!(remaining_bytes.len(), 0);
+
+    // check content
+    assert!(test_struct.a);
+    assert_eq!(test_struct.b, 2);
+
+    // write back and check
+    assert_eq!(buffer, test_struct.to_bytes().expect("encode error"));
+
+    test_struct.b = 0xFFFF;
+    assert!(test_struct.to_bytes().is_ok());
+}
+
+#[test]
+fn check_little_unsigned_u16_decode_encode_positive_value() {
+    #[derive(Debug, PartialEq, Default, Clone, DekuRead, DekuWrite)]
+    pub struct TestStruct {
+        #[deku(bits = "1")]
+        pub a: bool,
+        #[deku(pad_bits_before = "15", bits = "16", endian = "little")]
+        pub b: u16,
+    }
+
+    let buffer = vec![0b10000000, 0b00000000, 0b00000010, 0b00000000];
+
+    let ((remaining_bytes, offset), mut test_struct) =
+        TestStruct::from_bytes((&buffer, 0)).expect("decoder error");
+
+    // everything consumed?
+    assert_eq!(offset, 0);
+    assert_eq!(remaining_bytes.len(), 0);
+
+    // check content
+    assert!(test_struct.a);
+    assert_eq!(test_struct.b, 2);
+
+    // write back and check
+    assert_eq!(buffer, test_struct.to_bytes().expect("encode error"));
+
+    test_struct.b = 0xFFFF;
+    assert!(test_struct.to_bytes().is_ok());
+}
+
+#[test]
+fn check_big_signed_i16_decode_encode_positive_negative_value() {
+    #[derive(Debug, PartialEq, Default, Clone, DekuRead, DekuWrite)]
+    pub struct TestStruct {
+        #[deku(bits = "1")]
+        pub a: bool,
+        #[deku(pad_bits_before = "15", bits = "16", endian = "big")]
+        pub b: i16,
+    }
+
+    let buffer = vec![0b10000000, 0b00000000, 0b00000000, 0b00000010];
+
+    let ((remaining_bytes, offset), mut test_struct) =
+        TestStruct::from_bytes((&buffer, 0)).expect("decoder error");
+
+    // everything consumed?
+    assert_eq!(offset, 0);
+    assert_eq!(remaining_bytes.len(), 0);
+
+    // check content
+    assert!(test_struct.a);
+    assert_eq!(test_struct.b, 2);
+
+    // write back and check
+    assert_eq!(buffer, test_struct.to_bytes().expect("encode error"));
+
+    test_struct.b = i16::MIN;
+    assert!(test_struct.to_bytes().is_ok());
+
+    test_struct.b = i16::MAX;
+    assert!(test_struct.to_bytes().is_ok());
+}
+
+#[test]
+fn check_little_signed_i16_decode_encode_positive_negative_value() {
+    #[derive(Debug, PartialEq, Default, Clone, DekuRead, DekuWrite)]
+    pub struct TestStruct {
+        #[deku(bits = "1")]
+        pub a: bool,
+        #[deku(pad_bits_before = "15", bits = "16", endian = "little")]
+        pub b: i16,
+    }
+
+    let buffer = vec![0b10000000, 0b00000000, 0b00000010, 0b00000000];
+
+    let ((remaining_bytes, offset), mut test_struct) =
+        TestStruct::from_bytes((&buffer, 0)).expect("decoder error");
+
+    // everything consumed?
+    assert_eq!(offset, 0);
+    assert_eq!(remaining_bytes.len(), 0);
+
+    // check content
+    assert!(test_struct.a);
+    assert_eq!(test_struct.b, 2);
+
+    // write back and check
+    assert_eq!(buffer, test_struct.to_bytes().expect("encode error"));
+
+    test_struct.b = i16::MIN;
+    assert!(test_struct.to_bytes().is_ok());
+
+    test_struct.b = i16::MAX;
+    assert!(test_struct.to_bytes().is_ok());
+}

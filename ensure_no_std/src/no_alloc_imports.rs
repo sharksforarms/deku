a37cc7492feeb39ae4c36294b21e46bc89541b03
Matt Johnston <matt@codeconstruct.com.au>
2025-07-16 11:38:54 +0800

    Allow ensure_no_std to run without alloc

--- ensure_no_std/src/no_alloc_imports.rs
+++ ensure_no_std/src/no_alloc_imports.rs
@@ -2,29 +2,27 @@ use deku::prelude::*;
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
 struct DekuTest {
-    #[deku(bits = 5)]
     field_a: u8,
-    #[deku(bits = 3)]
     field_b: u8,
     count: u8,
 }
 
 pub fn rw() {
     #[allow(clippy::unusual_byte_groupings)]
-    let test_data: &[u8] = &[0b10101_101, 0x02];
-    let mut cursor = deku::no_std_io::Cursor::new(test_data);
+    let test_data: &[u8] = &[0xaa, 0xb0, 0x02];
 
     // Test reading
-    let (_rest, val) = DekuTest::from_reader((&mut cursor, 0)).unwrap();
+    let (_rest, val) = DekuTest::from_bytes((test_data, 0)).unwrap();
     assert_eq!(
         DekuTest {
-            field_a: 0b10101,
-            field_b: 0b101,
+            field_a: 0xaa,
+            field_b: 0xb0,
             count: 0x02,
         },
         val
     );
 
     // Test writing
-    let _val = val.to_bytes().unwrap();
+    let mut buf = [0; 20];
+    let _val = val.to_slice(&mut buf).unwrap();
 }
